880e3c16da648ff76195632ef37bd76badbb7640
KevinAqila <farrelahmad.kevinaqila@gmail.com>
2025-11-18 21:57:28 +0700

    fix: jwt payload incosistence

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -9,7 +9,7 @@ export const protectRoute = async (req, res, next) => {
       token = req.headers.authorization.split(' ')[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select('-password');
+      req.user = await User.findById(decoded.userId).select('-password');
       next();
     } catch (error) {
       return res.status(401).json({ message: "Not authorized, token failed" });
